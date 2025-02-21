90f58ac95cb94bc79f6627b1e339c42ceac94d80
Mike Söhner <mike.soehner@hlrs.de>
2025-02-21 18:18:16 +0100

    Added new test cases.
    Added test case for communicating multiple fundamentals of different
    types. Added test case for multiple views.

--- tests/send_tests.hpp
+++ tests/send_tests.hpp
@@ -12,32 +12,56 @@ TEST_CASE( "Send and Recv functionality", "[send_recv]" )
 
     SECTION("Sending multiple of the same fundamental type")
     {
+        int a{}, b{}, c{};
+        
         if (comm.get_rank() == 0)
         {
-            comm.send(1, Tag(0), 4, 5, 78);
+            a = 4, b = 5, c = 78;
+            comm.send(1, Tag(0), a, b, c);
         }
         else
         {
-            comm.recv(0, Tag(0), vec | std::views::take(3));
+            comm.recv(0, Tag(0), a, b, c);
 
-            REQUIRE(vec == std::vector<int> {4,5,78,0,0,0,0,0,0,0});
+            REQUIRE(a == 4);
+            REQUIRE(b == 5);
+            REQUIRE(c == 78);
         }   
     }
 
-    // SECTION("Sending multiple of different fundamental type") { }
+    SECTION("Sending multiple of different fundamental type") 
+    {
+        int a{};
+        unsigned long b{};
+        float c{};
+
+        if (comm.get_rank() == 0)
+        {
+            a = 4, b = 5, c = 78.0f;
+            comm.send(1, Tag(0), a, b, c);
+        }
+        else
+        {
+            comm.recv(0, Tag(0), a, b, c);
+
+            REQUIRE(a == 4);
+            REQUIRE(b == 5);
+            REQUIRE(c == 78.0f);
+        }   
+    }
 
     SECTION("Sending an entire stl container")
     {
         if (comm.get_rank() == 0)
         {
             std::iota(vec.begin(), vec.end(), 0);
-            comm.send(1, Tag(0), vec );
+            comm.send(1, Tag(0), vec);
 
             REQUIRE(vec == std::vector<int> {0,1,2,3,4,5,6,7,8,9});
         }
         else
         {
-            comm.recv(0, Tag(0), vec );
+            comm.recv(0, Tag(0), vec);
 
             REQUIRE(vec == std::vector<int> {0,1,2,3,4,5,6,7,8,9});
         }
@@ -57,6 +81,23 @@ TEST_CASE( "Send and Recv functionality", "[send_recv]" )
             comm.recv(0, Tag(0), vec | std::ranges::views::take(3));
 
             REQUIRE(vec == std::vector<int> {0,1,2,0,0,0,0,0,0,0});
+        }
+    }
+
+    SECTION("Sending multiple fractions of an stl container")
+    {
+        if (comm.get_rank() == 0)
+        {
+            std::iota(vec.begin(), vec.end(), 0);
+            comm.send(1, Tag(0), vec | std::ranges::views::take(3), vec | std::ranges::views::drop(7));
+
+            REQUIRE(vec == std::vector<int> {0,1,2,3,4,5,6,7,8,9});
+        }
+        else
+        {
+            comm.recv(0, Tag(0), vec | std::ranges::views::take(3), vec | std::ranges::views::drop(7));
+
+            REQUIRE(vec == std::vector<int> {0,1,2,0,0,0,0,7,8,9});
         }
     }
 }
