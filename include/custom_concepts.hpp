d3f8f5f045c4ef009169110167e8c134269f61de
Mike Söhner <mike.soehner@hlrs.de>
2025-03-12 16:20:53 +0100

    Bugfix: concept for underlying types of ranges.

--- include/custom_concepts.hpp
+++ include/custom_concepts.hpp
@@ -32,21 +32,13 @@ concept are_only_views = ((std::ranges::range<Vs> && std::movable<Vs> && std::ra
 template<typename... Rs>
 concept are_only_ranges = ((std::ranges::range<Rs> && std::movable<Rs> && !std::ranges::enable_view<Rs>) && ...);
 
-
-// checks if all types in the pack are ranges with the same underlying type
-template<typename T, typename U>
-constexpr bool free_are_same_types(T head0, U head1)
-{
-    return std::is_same_v<std::ranges::range_value_t<T>, std::ranges::range_value_t<U>>;
-}
-// checks if all types in the pack are ranges with the same underlying type
-template<typename T, typename U, typename... Ts>
-constexpr bool free_are_same_types(T head0, U head1, Ts...tail)
-{
-    return std::is_same_v<std::ranges::range_value_t<T>, std::ranges::range_value_t<U>> && are_same_types(head1, tail...);
-}
+ // checks if the underlying type of all ranges is the same
+template<typename T, typename... Ts>
+constexpr bool all_types_are_same = std::conjunction_v<std::is_same<
+                                            std::ranges::range_value_t<T>, 
+                                            std::ranges::range_value_t<Ts>>...>;
 
 template<typename... Ts>
-concept are_same_types = (free_are_same_types(Ts{}...));
+concept are_same_types = (all_types_are_same<Ts...>);
 
 #endif
