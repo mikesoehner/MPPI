d69be0cf97dc32065f4f2a97802e9f4de23684e4
Mike Söhner <mike.soehner@hlrs.de>
2025-06-04 20:45:41 +0200

    Add bandwidth benchmarks.
    Same as in the main branch.

--- benchmarks/bandwidth_benchmark_stl.cpp
+++ benchmarks/bandwidth_benchmark_stl.cpp
@@ -18,42 +18,44 @@ int main(int argc, char** argv)
     if(comm.get_rank() == 0)
         std::cout << "# OSU Inpired MPI Bandwidth Test\n";
 
-    std::vector<MPI_Datatype> mpi_types;
-    mpi_types.emplace_back(MPI_CHAR);
-    mpi_types.emplace_back(MPI_INT);
-    mpi_types.emplace_back(MPI_FLOAT);
-    
-    // loop through different datatypes that should be benchmarked
-    for (auto mpi_type : mpi_types)
-    {
-        int type_size {};
-        MPI_Type_size(mpi_type, &type_size);
+    auto benchmark = [&comm]<typename T>(T t)
+    {   
+        auto type_size = sizeof(T);
+
         size_t min_message_size = type_size;
         size_t max_message_size = 4194304;
 
-        char type_name[128];
-        int resultlen {};
-        MPI_Type_get_name(mpi_type, type_name, &resultlen);
         if (comm.get_rank() == 0)
         {
-            fprintf(stdout, "# Datatype: %s.\n", type_name);
+            if constexpr (std::is_same_v<T, char>)
+                fprintf(stdout, "# Datatype: char.\n");
+            else if constexpr (std::is_same_v<T, int>)
+                fprintf(stdout, "# Datatype: int.\n");
+            else if constexpr (std::is_same_v<T, double>)
+                fprintf(stdout, "# Datatype: double.\n");
+            else
+            {
+                MPI_Abort(MPI_COMM_WORLD, 1);
+                return 1;
+            }
+
             fprintf(stdout, "%-*s", 10, "# Size");
             fprintf(stdout, "%*s", 20, "Bandwidth (MB/s)\n");
         }
         
-            // loop through message sizes
+        // loop through message sizes
         for (size_t size = min_message_size; size <= max_message_size; size *= 2)
         {
             // get number of elements in message
             auto nb_elements = size / type_size;
-            //
-            constexpr auto window_size = 64;
-            MPI_Request request[window_size];
-            MPI_Status reqstat[window_size];
-        
-            std::vector<std::byte> send_buf(size);
-            std::vector<std::byte> recv_buf(size);
 
+            constexpr auto window_size = 64;
+            std::array<mppi::Request, window_size> requests;
+            MPI_Status reqstat[window_size];
+
+            std::vector<T> send_buf(nb_elements);
+            std::vector<T> recv_buf(nb_elements);
+            //
             MPI_Barrier(MPI_COMM_WORLD);
 
             double time_total = 0.0;
@@ -66,14 +68,13 @@ int main(int argc, char** argv)
                     double time_start = MPI_Wtime();
 
                     for (size_t j = 0; j < window_size; j++)
-                        MPI_Isend(send_buf.data(), nb_elements, mpi_type, 1, 100, MPI_COMM_WORLD, request + j);
+                        requests[j] = comm.isend(1, mppi::Tag(99), send_buf | std::ranges::views::all);
 
-                    MPI_Waitall(window_size, request, reqstat);
-                    // for (size_t j = 0; j < window_size; j++)
-                    //     MPI_Wait(request+j, MPI_STATUS_IGNORE);
-
+                    comm.waitall(requests);
+                    // MPI_Waitall(window_size, request, reqstat);
                     char c;
-                    MPI_Recv(&c, 1, MPI_CHAR, 1, 101, MPI_COMM_WORLD, &reqstat[0]);
+                    comm.recv(1, mppi::Tag(101), c);
+                    // MPI_Recv(recv_buf[0], 1, MPI_CHAR, 1, 101, MPI_COMM_WORLD, &reqstat[0]);
 
                     double time_end = MPI_Wtime();
                     time_total += time_end - time_start;
@@ -81,14 +82,14 @@ int main(int argc, char** argv)
                 else if (comm.get_rank() == 1)
                 {
                     for (size_t j = 0; j < window_size; j++)
-                        MPI_Irecv(recv_buf.data(), nb_elements, mpi_type, 0, 100, MPI_COMM_WORLD, request + j);
+                        requests[j] = comm.irecv(0, mppi::Tag(99), recv_buf | std::ranges::views::all);
                     
-                    MPI_Waitall(window_size, request, reqstat);
-                    // for (size_t j = 0; j < window_size; j++)
-                    //     MPI_Wait(request+j, MPI_STATUS_IGNORE);
-                    
+                    comm.waitall(requests);
+                    // MPI_Waitall(window_size, request, reqstat);
                     char c = 'd';
-                    MPI_Send(&c, 1, MPI_CHAR, 0, 101, MPI_COMM_WORLD);
+                    comm.send(0, mppi::Tag(101), c);
+                    // MPI_Send(send_buf[0], 1, MPI_CHAR, 0, 101, MPI_COMM_WORLD);
+
                 }
             }
 
@@ -102,7 +103,14 @@ int main(int argc, char** argv)
                 fflush(stdout);
             }
         }
-    }
+    };
+
+    auto wrapper = [benchmark]<typename... Ts>(Ts... types)
+    {
+        (benchmark(types), ...);
+    };
+
+    wrapper(char{}, int{}, double{});
 
     MPI_Finalize();
     return 0;
